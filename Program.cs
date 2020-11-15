8b67daf2784185727be66f598f1900a33b6b61ab
Saahil Claypool <saahil@claypools.org>
2020-11-15 14:37:54 -0500

    Convert to top level program

--- Program.cs
+++ Program.cs
@@ -3,35 +3,31 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Extensions;
+using UnsplashBulkDownload;
 
-namespace UnsplashBulkDownload {
-    class Program {
-        private static string OutputFolder {
-            get {
-                var folder = Environment.GetEnvironmentVariable("BACKGROUND_FOLDER") ?? "./Photos/";
-                if (folder.EndsWith("/") is not true) {
-                    folder += "/";
-                }
-                return folder;
-            }
-        }
-        private static bool CreateFolder => (Environment.GetEnvironmentVariable("CREATE_FOLDER") ?? "true") == "true";
-        private static bool ClearFolder => (Environment.GetEnvironmentVariable("CLEAR_FOLDER") ?? "true") == "true";
-        static async Task Main(string[] args) {
-            if (!Directory.Exists(OutputFolder) && CreateFolder is true) {
-                Directory.CreateDirectory(OutputFolder);
-            }
-            if (ClearFolder is true) {
-                foreach (var file in Directory.EnumerateFiles(OutputFolder)) {
-                    File.Delete(file);
-                }
-            }
+static string GetOutputFolder() {
+    var folder = Environment.GetEnvironmentVariable("BACKGROUND_FOLDER") ?? "./Photos/";
+    if (folder.EndsWith("/") is not true) {
+        folder += "/";
+    }
+    return folder;
+}
 
-            var api = new UnsplashApi();
-            var photos = await UnsplashApi.GetCollectionPhotos("319663");
-            Console.WriteLine(photos.ToJson(pretty: true));
-            var downloads = photos.Select(photo => UnsplashApi.DownloadPhoto(photo, OutputFolder));
-            await Task.WhenAll(downloads);
-        }
+string OutputFolder = GetOutputFolder();
+bool CreateFolder = (Environment.GetEnvironmentVariable("CREATE_FOLDER") ?? "true") == "true";
+bool ClearFolder = (Environment.GetEnvironmentVariable("CLEAR_FOLDER") ?? "true") == "true";
+
+if (!Directory.Exists(OutputFolder) && CreateFolder is true) {
+    Directory.CreateDirectory(OutputFolder);
+}
+if (ClearFolder is true) {
+    foreach (var file in Directory.EnumerateFiles(OutputFolder)) {
+        File.Delete(file);
     }
 }
+
+var api = new UnsplashApi();
+var photos = await UnsplashApi.GetCollectionPhotos("319663");
+Console.WriteLine(photos.ToJson(pretty: true));
+var downloads = photos.Select(photo => UnsplashApi.DownloadPhoto(photo, OutputFolder));
+await Task.WhenAll(downloads);
