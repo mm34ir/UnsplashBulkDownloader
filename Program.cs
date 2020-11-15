94e5eaa2ffdfd2ad75b9bdfb85c124970c88d55f
Saahil Claypool <saahil@claypools.org>
2020-11-15 14:23:41 -0500

    Made file creates and clears background folder
    TODO: collection id as script variable

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Extensions;
 
 namespace UnsplashBulkDownload {
     class Program {
+        private static string OutputFolder {
+            get {
+                var folder = Environment.GetEnvironmentVariable("BACKGROUND_FOLDER") ?? "./Photos/";
+                if (folder.EndsWith("/") is not true) {
+                    folder += "/";
+                }
+                return folder;
+            }
+        }
+        private static bool CreateFolder => (Environment.GetEnvironmentVariable("CREATE_FOLDER") ?? "true") == "true";
+        private static bool ClearFolder => (Environment.GetEnvironmentVariable("CLEAR_FOLDER") ?? "true") == "true";
         static async Task Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            if (!Directory.Exists(OutputFolder) && CreateFolder is true) {
+                Directory.CreateDirectory(OutputFolder);
+            }
+            if (ClearFolder is true) {
+                foreach (var file in Directory.EnumerateFiles(OutputFolder)) {
+                    File.Delete(file);
+                }
+            }
+
             var api = new UnsplashApi();
-            var results = await UnsplashApi.GetCollectionPhotos("319663");
-            Console.WriteLine(results.ToJson(pretty: true));
+            var photos = await UnsplashApi.GetCollectionPhotos("319663");
+            Console.WriteLine(photos.ToJson(pretty: true));
+            var downloads = photos.Select(photo => api.DownloadPhoto(photo, OutputFolder));
+            await Task.WhenAll(downloads);
         }
     }
 }
